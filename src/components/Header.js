618bab61527d8e28db614eac7662e3a7f0e7770b
Garrett Nay <g@garrettnay.com>
2019-03-30 22:02:49 -0600

    Start styling the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,17 @@ import Portal from './Portal'
 
 export default function Header() {
   return (
-    <Portal selector="body > header" className="bg-black text-white">
-      <h1>Quizboards!</h1>
+    <Portal
+      selector="body > header"
+      className="flex flex-row justify-between items-center bg-black text-white px-4 py-2"
+    >
+      <h1 className="text-3xl">Quizboards!</h1>
+      <div>
+        <a href="/">My Boards</a>
+        <button className="bg-green-600 hover:bg-green-700 px-2 py-4 rounded">
+          Create New Board
+        </button>
+      </div>
     </Portal>
   )
 }
